4534ab7f79da7af76e49f8dc48751ca6c2dea6bd
LokiGoiano <lorddavi19@gmail.com>
2024-10-02 16:01:50 -0300

    tabela

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ const App: React.FC = () => {
     // Carregar agendamentos ao carregar a página
     const fetchBookings = async () => {
       try {
-        const response = await api.get(`/Appointments`);
+        const response = await api.get(`/Appointment`);
         const data = response.data;
 
         const loadedBookings: { [key: number]: Booking[] } = {};
